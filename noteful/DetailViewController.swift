4478de0143a329eb7c31a2fed478ad1e8f175d57
Brad Chesworth <bradley.chesworth@bbc.co.uk>
2020-05-31 12:22:26 +0100

    DetailView has textView which displays notes information

--- noteful/DetailViewController.swift
+++ noteful/DetailViewController.swift
@@ -9,11 +9,38 @@
 import UIKit
 
 class DetailViewController: UIViewController {
+    
+    @IBOutlet weak var noteBody: UITextView!
+    
+    var note: Note?
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         navigationItem.largeTitleDisplayMode = .never
+        
+        let notificationCenter = NotificationCenter.default
+        notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillHideNotification, object: nil)
+        notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
+        
+        noteBody.text = note?.body
     }
+    
+    @objc func adjustForKeyboard(notification: Notification) {
+        let userInfo = notification.userInfo!
 
+        let keyboardScreenEndFrame = (userInfo[UIResponder.keyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
+        let keyboardViewEndFrame = view.convert(keyboardScreenEndFrame, from: view.window)
+        
+        if notification.name == UIResponder.keyboardWillHideNotification {
+            noteBody.contentInset = UIEdgeInsets.zero
+        } else {
+            noteBody.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: keyboardViewEndFrame.height, right: 0)
+        }
+
+        noteBody.scrollIndicatorInsets = noteBody.contentInset
+
+        let selectedRange = noteBody.selectedRange
+        noteBody.scrollRangeToVisible(selectedRange)
+    }
 }
--- noteful/TableViewController.swift
+++ noteful/TableViewController.swift
@@ -33,6 +33,7 @@ class TableViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         if let vc = storyboard?.instantiateViewController(identifier: "Detail") as? DetailViewController {
+            vc.note = notes[indexPath.row]
             navigationController?.pushViewController(vc, animated: true)
         }
     }
